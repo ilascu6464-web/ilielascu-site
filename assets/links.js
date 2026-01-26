4426b042c231431d5970995e1837726cda42d5e3
ILIE LASCU <ilascu6464@gmail.com>
2026-01-26 16:16:01 +0100

    Added Echoes

--- assets/links.js
+++ assets/links.js
@@ -1,67 +1,68 @@
-/* AICI ADMINISTREZI LINK-URILE DE TIKTOK */
-
-const tiktokLinks = {
-    // --- INCURSIUNE ÎN IMPOSIBIL (SOUNDTRACK) ---
-    "Xorax on the wings of a swan": "https://www.tiktok.com/@ilascu99/video/7591820681294138662",
-    "NINA": "https://www.tiktok.com/@ilascu99/video/7592540560452259094",
-    "Incursion into the impossible": "https://www.tiktok.com/@ilascu99/video/7593256966085201155",
-    "El Guardián de la Luna": "https://www.tiktok.com/@ilascu99/video/7593810172376616214",
+var tiktokLinks = {
+    // --- INCURSIUNE IN IMPOSIBIL ---
+    "Xorax on the wings of a swan": "https://www.tiktok.com/@ilascu99/video/7417537604464872737",
+    "NINA": "https://www.tiktok.com/@ilascu99/video/7419131641042996513",
+    "Incursion into the impossible": "https://www.tiktok.com/@ilascu99/video/7414902179837431073",
+    "El Guardián de la Luna": "https://www.tiktok.com/@ilascu99/video/7421319253459635488",
 
     // --- BIOGRAPHY IN SOUND ---
-    "Number One": "https://www.tiktok.com/@ilascu99/video/7594852443175832854",
-    "Look what you've done to me": "https://www.tiktok.com/@ilascu99/video/7595142165840465175",
-    "Got my hands on my phone": "https://www.tiktok.com/@ilascu99/video/7598227681427361047",
-    "Daddy's on my side": "https://www.tiktok.com/@ilascu99/video/7596616907680255254",
-    "Don’t be a Blackrock": "https://www.tiktok.com/@ilascu99/video/7598823580541013270",
-    "FIVE BEATS FORWARD": "https://www.tiktok.com/@ilascu99/video/7599207400578501910",
+    "Number One": "https://www.tiktok.com/@ilascu99/video/7428383838423371040",
+    "Look what you've done to me": "https://www.tiktok.com/@ilascu99/video/7429107955518917920",
+    "Got my hands on my phone": "https://www.tiktok.com/@ilascu99/video/7432135245806931222",
+    "Daddy's on my side": "https://www.tiktok.com/@ilascu99/video/7433221971715558678",
+    "Don’t be a Blackrock": "https://www.tiktok.com/@ilascu99/video/7434720932225879318",
+    "FIVE BEATS FORWARD": "https://www.tiktok.com/@ilascu99/video/7437340237937446166",
+    "Echoes of the Giants-Tribute to the Titans": "https://www.tiktok.com/@ilascu99/video/7599686551995452694",
 
     // --- NEW SINGLES ---
-    "Barrio Tango": "https://www.tiktok.com/@ilascu99/video/7591480119864970518",
-    "Please, love me too": "https://www.tiktok.com/@ilascu99/video/7588412312445340950",
-    "HAI LA JOC DE ANUL NOU": "https://www.tiktok.com/@ilascu99/video/7586720494544604438",
-    "One Last Christmas": "https://www.tiktok.com/@ilascu99/video/7586285306152848662",
-    "The Bread She Never Ate": "https://www.tiktok.com/@ilascu99/video/7585464981445954838",
+    "Barrio Tango": "https://www.tiktok.com/@ilascu99/video/7440656096504286486",
+    "Please, love me too": "https://www.tiktok.com/@ilascu99/video/7442171279861058838",
+    "HAI LA JOC DE ANUL NOU": "https://www.tiktok.com/@ilascu99/video/7452610582537833750",
+    "One Last Christmas": "https://www.tiktok.com/@ilascu99/video/7450702434520517910",
+    "The Bread She Never Ate": "https://www.tiktok.com/@ilascu99/video/7453715830912306454",
 
-    // --- HAI LA JOC (TITLURI SPECIALE) ---
-    "Clap Your Hands": "https://www.tiktok.com/@ilascu99/video/7588160544671026454", 
-    "Buenos Aires-Taste of Colour": "https://www.tiktok.com/@ilascu99/video/7570749044495043862?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
+    // --- THE BEST OF ---
+    "Too late for crying": "https://www.tiktok.com/@ilascu99/video/7430983083651517718",
+    "Țuică cu Tsatsiki": "https://www.tiktok.com/@ilascu99/video/7424647317669285152",
+    "Rakia i Suze, Frați pe Vecie": "https://www.tiktok.com/@ilascu99/video/7420230230758526240",
+    "Țup-Țup-Țup": "https://www.tiktok.com/@ilascu99/video/7423549298316135712",
+
+    // --- HAI LA JOC ---
+    "Clap Your Hands": "https://www.tiktok.com/@ilascu99/video/7425026909563276576",
+    "Ridică-te grindă-n sus": "https://www.tiktok.com/@ilascu99/video/7416766453916601633",
+    "RUMBA-ȚUMBA": "https://www.tiktok.com/@ilascu99/video/7418738374241602848",
+    "Hermannstadt tanzt mit Bruckenthal": "https://www.tiktok.com/@ilascu99/video/7415286460145405217",
+    "Buenos Aires-Taste of Colour": "https://www.tiktok.com/@ilascu99/video/7427976826627722528",
 
     // --- JAZZ SCRATCHES ---
-    "The Companion": "https://www.tiktok.com/@ilascu99/video/7584363347429346582?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Please, like Me": "https://www.tiktok.com/@ilascu99/video/7583739915582590230?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Jazz Scratches": "https://www.tiktok.com/@ilascu99/video/7584766601786887446?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
+    "The Companion": "https://www.tiktok.com/@ilascu99/video/7426149950791781664",
+    "Please, like Me": "https://www.tiktok.com/@ilascu99/video/7425392683226270998",
+    "Jazz Scratches": "https://www.tiktok.com/@ilascu99/video/7426861614088424736",
 
     // --- HOP & JAZZ ---
-    "YUGOs BRAVEHEARTS": "https://www.tiktok.com/@ilascu99/video/7578835720710769942?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Țuică cu Tsatsiki": "https://www.tiktok.com/@ilascu99/video/7579524812914101526?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Too late for crying": "https://www.tiktok.com/@ilascu99/video/7585464981445954838", 
+    "YUGOs BRAVEHEARTS": "https://www.tiktok.com/@ilascu99/video/7425785089306168608",
 
     // --- ANALOG CONFLICT ---
-    "Hermannstadt tanzt mit Bruckenthal": "https://www.tiktok.com/@ilascu99/video/7572601597998435606?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Focul de sub Podul Minciunilor": "https://www.tiktok.com/@ilascu99/video/7571878285618056470?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Symphonia Residualis": "https://www.tiktok.com/@ilascu99/video/7574053234881465622?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Ridică-te grindă-n sus": "https://www.tiktok.com/@ilascu99/video/7575167370990259478?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Țup-Țup-Țup": "https://www.tiktok.com/@ilascu99/video/7576184453224041750?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Rakia i Suze, Frați pe Vecie": "https://www.tiktok.com/@ilascu99/video/7577471339599006998?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "RUMBA-ȚUMBA": "https://www.tiktok.com/@ilascu99/video/7578412209848470806?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
+    "Focul de sub Podul Minciunilor": "https://www.tiktok.com/@ilascu99/video/7416041641011236128",
+    "Symphonia Residualis": "https://www.tiktok.com/@ilascu99/video/7416399187358190881",
 
     // --- ORBITA ZERO ---
-    "Prietenul meu ARGO": "https://www.tiktok.com/@ilascu99/video/7571032500143688982?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Clara's Song": "https://www.tiktok.com/@ilascu99/video/7565854579527666966?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Taste of Life": "https://www.tiktok.com/@ilascu99/video/7565890023040519446?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Taste of Light": "https://www.tiktok.com/@ilascu99/video/7565901259870358806?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Taste of Truth": "https://www.tiktok.com/@ilascu99/video/7565908997056761110?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "ARGO-9's Dream": "https://www.tiktok.com/@ilascu99/video/7567667916724866326?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Nodo del Plata": "https://www.tiktok.com/@ilascu99/video/7569077155368373526?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Matías Verón": "https://youtu.be/UR8MI6MHnlc",
-    "Taste of Colour": "https://www.tiktok.com/@ilascu99/video/7570749044495043862?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
+    "Prietenul meu ARGO": "https://www.tiktok.com/@ilascu99/video/7409292857413684513",
+    "Clara's Song": "https://www.tiktok.com/@ilascu99/video/7411931346571595040",
+    "Taste of Life": "https://www.tiktok.com/@ilascu99/video/7412297123912994080",
+    "Taste of Light": "https://www.tiktok.com/@ilascu99/video/7412643516104101153",
+    "Taste of Truth": "https://www.tiktok.com/@ilascu99/video/7413038670833077536",
+    "ARGO-9's Dream": "https://www.tiktok.com/@ilascu99/video/7413411030027275553",
+    "Nodo del Plata": "https://www.tiktok.com/@ilascu99/video/7413757656910728480",
+    "Matías Verón": "https://www.tiktok.com/@ilascu99/video/7414152501068221728",
+    "Taste of Colour": "https://www.tiktok.com/@ilascu99/video/7414513904622718241",
 
     // --- ABOUT THE MOTH ---
-    "About the Moth": "https://www.tiktok.com/@ilascu99/video/7524991977146895638?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Loving my little Moth": "https://www.tiktok.com/@ilascu99/video/7526095860166380822?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Jack": "https://www.tiktok.com/@ilascu99/video/7525836782492077334?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Sadhguru's Steps": "https://www.tiktok.com/@ilascu99/video/7525386809715592470?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Siberian Wind": "https://www.tiktok.com/@ilascu99/video/7525665702783569174?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Silenus": "https://www.tiktok.com/@ilascu99/video/7525830397410790678?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166",
-    "Amphibian": "https://www.tiktok.com/@ilascu99/video/7526221989309074710?is_from_webapp=1&sender_device=pc&web_id=7574081715187910166"
+    "About the Moth": "https://www.tiktok.com/@ilascu99/video/7410069357591104800",
+    "Loving my little Moth": "https://www.tiktok.com/@ilascu99/video/7410406856427326752",
+    "Jack": "https://www.tiktok.com/@ilascu99/video/7410776735517101344",
+    "Sadhguru's Steps": "https://www.tiktok.com/@ilascu99/video/7411162464013470992",
+    "Siberian Wind": "https://www.tiktok.com/@ilascu99/video/7411548680323992864",
+    "Silenus": "https://www.tiktok.com/@ilascu99/video/7411904732001553696",
+    "Amphibian": "https://www.tiktok.com/@ilascu99/video/7412271379769970001"
 };
